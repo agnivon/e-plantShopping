d501ab4683531c72899a68301db0a659e43d29be
agnivon <agnivon@gmail.com>
2025-02-26 21:24:57 +0530

    fix: used BASE_URL env var

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 import { Product } from "./types/product.types";
 
 export async function fetchAllProducts(): Promise<Product[]> {
-  const response = await fetch("/data/products.json");
+  const response = await fetch(`${import.meta.env.BASE_URL}data/products.json`);
   const data = await response.json();
   return data;
 }
