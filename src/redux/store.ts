2e2823ae2fcce498dd8602ac5e88c52d7fa45900
agnivon <agnivon@gmail.com>
2025-02-26 21:40:14 +0530

    fix: redux persist config

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,15 +4,18 @@ import storage from "redux-persist/lib/storage";
 import { productSlice } from "./slices/product.slice";
 import { shoppingCartSlice } from "./slices/shopping-cart.slice";
 
-const persistConfig = {
-  key: "root",
+const persistConfig = (key: string) => ({
+  key,
   storage,
-};
+});
 
 export const store = configureStore({
   reducer: {
-    products: persistReducer(persistConfig, productSlice.reducer),
-    shoppingCart: persistReducer(persistConfig, shoppingCartSlice.reducer),
+    products: persistReducer(persistConfig("products"), productSlice.reducer),
+    shoppingCart: persistReducer(
+      persistConfig("shoppingCart"),
+      shoppingCartSlice.reducer
+    ),
   },
 });
 export const persistor = persistStore(store);
